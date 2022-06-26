7a5b0eb01cf4177229d540b04692ab7556121c3b
Herr Erde <Natodamm@hotmail.com>
2022-06-26 04:45:14 +0200

    website fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
 setTimeout(() => {
-    document.getElementById("hate").style.opacity = Math.random() > .9 ? ".1" : "0"
-    document.body.querySelector("main").style.opacity = "1"
-}, 100)
+  document.getElementById('hate').style.opacity =
+    Math.random() > 0.9 ? '.1' : '0';
+  document.body.querySelector('main').style.opacity = '1';
+}, 100);
 setInterval(() => {
-    document.getElementById("title").style.fontSize = ((window.innerHeight + (window.innerWidth)) / 30) + "px"
-    document.getElementById("subt").style.fontSize = ((window.innerHeight + window.innerWidth) / 50) + "px"
-    document.getElementById("langs").style.fontSize = ((window.innerHeight + window.innerWidth) / 70) + "px"
-    for (el of document.querySelectorAll("div.icons>a")) {
-        el.style.marginRight = (((window.innerHeight + (window.innerWidth)) / 30) / (3)) + "px"
-    }
+  document.getElementById('title').style.fontSize =
+    (window.innerHeight + window.innerWidth) / 30 + 'px';
+  document.getElementById('subt').style.fontSize =
+    (window.innerHeight + window.innerWidth) / 50 + 'px';
+  document.getElementById('langs').style.fontSize =
+    (window.innerHeight + window.innerWidth) / 70 + 'px';
+  for (el of document.querySelectorAll('div.icons>a')) {
+    el.style.marginRight =
+      (window.innerHeight + window.innerWidth) / 30 / 3 + 'px';
+  }
 }, 100);
